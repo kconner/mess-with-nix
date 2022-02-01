d0057bfbc4d0b660b9f315f1103236d5a6d61a79
Kevin Conner <connerk@gmail.com>
2022-01-31 20:11:44 -0500

    Pill 8 part 3

--- pill8/hello.nix
+++ pill8/hello.nix
@@ -1,9 +1,7 @@
-with (import <nixpkgs> {});
-derivation {
+let
+  pkgs = import <nixpkgs> {};
+  mkDerivation = import ./autotools.nix pkgs;
+in mkDerivation {
   name = "hello";
-  builder = "${bash}/bin/bash";
-  args = [ ./builder.sh ];
-  buildInputs = [ gnutar gzip gnumake clang clang.bintools.bintools_bin coreutils gawk gnused gnugrep ];
   src = ./hello-2.10.tar.gz;
-  system = builtins.currentSystem;
 }

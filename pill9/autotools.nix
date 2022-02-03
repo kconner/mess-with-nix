799f19661cf903e4ecfab8f62929ef67781ed780
Kevin Conner <connerk@gmail.com>
2022-02-02 22:36:46 -0500

    Pill 9

---
+++ pill9/autotools.nix
@@ -0,0 +1,11 @@
+pkgs: attrs:
+  with pkgs;
+  let defaultAttrs = {
+    builder = "${bash}/bin/bash";
+    args = [ ./builder.sh ];
+    baseInputs = [ gnutar gzip gnumake clang clang.bintools.bintools_bin coreutils gawk gnused gnugrep findutils patchelf ];
+    buildInputs = [];
+    system = builtins.currentSystem;
+  };
+  in
+  derivation (defaultAttrs // attrs)

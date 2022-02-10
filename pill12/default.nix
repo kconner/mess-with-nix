1b1d252214f549dfd04d2fe9f6148c204b954e9a
Kevin Conner <connerk@gmail.com>
2022-02-09 23:09:30 -0500

    Pill 12 part 3

--- pill12/default.nix
+++ pill12/default.nix
@@ -1,4 +1,11 @@
-{
-  hello = import ./hello.nix;
-  graphviz = import ./graphviz.nix;
+let
+  pkgs = import <nixpkgs> {};
+  mkDerivation = import ./autotools.nix pkgs;
+in with pkgs; {
+  hello = import ./hello.nix { inherit mkDerivation; };
+  graphviz = import ./graphviz.nix { inherit mkDerivation lib gd pkg-config; };
+  graphvizCore = import ./graphviz.nix {
+    inherit mkDerivation lib gd pkg-config;
+    gdSupport = false;
+  };
 }
--- pill12/graphviz.nix
+++ pill12/graphviz.nix
@@ -1,12 +1,20 @@
-let
-  pkgs = import <nixpkgs> {};
-  mkDerivation = import ./autotools.nix pkgs;
-in mkDerivation {
+{
+  mkDerivation,
+  lib,
+  gdSupport ? false, # true won't build on macOS 12
+  gd,
+  pkg-config
+}:
+
+mkDerivation {
   name = "graphviz";
   src = ./graphviz-2.49.3.tar.gz;
-  # buildInputs = with pkgs; [
-    # pkg-config
-    # (pkgs.lib.getLib gd)
-    # (pkgs.lib.getDev gd)
-  # ];
+  buildInputs =
+    if gdSupport
+      then [
+        pkg-config
+        (lib.getLib gd)
+        (lib.getDev gd)
+      ]
+    else [];
 } 
